0a4440c36a534ca0156c9ff67340e5af445d97e7
Gromovoi Stanislav <gromila12@rambler.ru>
2023-06-02 18:00:28 +0600

    array

---
+++ array.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a [3]int
+	b := [3]int{1, 2, 3}
+	c := [...]int{1, 2, 3} // длина массива будет определена Go в зависимости от количества указанных при инициализации значений
+	d := [3]int{1: 12} //явно указать значение, которое должно быть присвоено элементу массива с определенным индексом
+	e := [6]int{1: 12, 2: 11, 5: 3}
+
+	fmt.Println(a) // [0 0 0]
+	fmt.Println(b) // [1 2 3]
+	fmt.Println(c) // [1 2 3]
+	fmt.Println(d) // [0 12 0]
+	fmt.Println(e) // [0 12 11 0 0 3]
+
+
+	fmt.Println(b == c) // true
+	fmt.Println(a == b) // false
+	fmt.Println(d == e) // invalid operation: d == e (mismatched types [3]int and [6]int)
+}
